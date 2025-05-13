917619bc6a626a25b9425fe6e6e3423a3df2228b
Akim <akimduvanov@gmail.com>
2025-05-13 20:22:56 +0200

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,33 @@
+'use strict';
+
+class Storage {
+  #items = [];
+  constructor(items) {
+    this.#items = items;
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    if (this.#items.includes(itemToRemove)) {
+      const arr1 = this.#items.slice(0, this.#items.indexOf(itemToRemove));
+      const arr2 = this.#items.slice(this.#items.indexOf(itemToRemove) + 1, this.#items.length);
+      this.#items = arr1.concat(arr2);
+    }
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
